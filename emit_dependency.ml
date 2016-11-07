00573924b2ec6518dc7027af0b54971a52d05c10
sueki743 <sueki743@users.noreply.github.com>
2016-11-07 22:33:36 +0900

    minrt.mlの関数間の依存関係解析 無視する関数を指定

--- emit_dependency.ml
+++ emit_dependency.ml
@@ -1,5 +1,11 @@
 open Syntax
 
+module NodeSet =
+	Set.Make
+		(struct
+			type t = string
+			let compare = compare
+		end)
 module EdgeSet =
 	Set.Make
 		(struct
@@ -7,13 +13,32 @@ module EdgeSet =
 			let compare = compare
 		end)
 
+let input_nodes ic =
+	let rec iter_input_nodes nodes =
+		try
+			let line = input_line ic in
+			if line.[0] = '#' then iter_input_nodes nodes
+			                  else iter_input_nodes (NodeSet.add line nodes)
+		with
+		| End_of_file -> nodes
+		| Invalid_argument _ -> iter_input_nodes nodes
+	in iter_input_nodes NodeSet.empty
+
 let f oc e =
+	let ignore_nodes =
+		let ic = open_in "test/minrt.ignore" in
+		let ignore_nodes = input_nodes ic in
+			close_in ic; ignore_nodes
+	in
 	let rec g (nodes, edges) = function
+		| LetRec({name=(x0,t); args=yts; body=e1}, e2) when NodeSet.mem x0 ignore_nodes ->
+			g (nodes, edges) e2
 		| LetRec({name=(x0,t); args=yts; body=e1}, e2) ->
 			let nodes = x0::nodes in
 			let rec h edges = function
 				| App(e,es) ->
 					(match e with
+					| Var(x) when NodeSet.mem x ignore_nodes -> List.fold_left h edges es
 					| Var(x) -> List.fold_left h (EdgeSet.add (x0, x) edges) es
 					| _ -> failwith "App(not Var, _)"
 					)
