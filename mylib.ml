86e12b5a5a4bdc41d9fb71284b59023c58b1385b
asaike <kentaro.asai.is@gmail.com>
2016-11-29 13:19:20 +0900

    Update mylib.ml

--- mylib.ml
+++ mylib.ml
@@ -8,7 +8,7 @@ let rec atan x =
 	x -. x *. x *. x /. 3.0 +. x *. x *. x *. x *. x /. 5.0
 in
 let rec floor x =
-	float_of_int (int_of_float (x +. 0.5))
+	float_of_int (int_of_float (x -. 0.5))
 in
 
 
