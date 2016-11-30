4633816081fd65e6a9651004605725edb8568f51
sueki743 <sueki743@users.noreply.github.com>
2016-12-01 03:26:46 +0900

    minrt.ml 出力周り

--- rayracer/minrt.ml
+++ rayracer/minrt.ml
@@ -1921,13 +1921,22 @@ in
 let rec write_ppm_header _ =
   (
     print_char 80; (* 'P' *)
-    print_char (48 + 3); (* +6 if binary *) (* 48 = '0' *)
+    print_char (48 + 6); (* +6 if binary *) (* 48 = '0' *)
     print_char 10;
-    print_int image_size.(0);
+    (* print_int image_size.(0); *)
+    print_char (48 + 1);
+    print_char (48 + 2);
+    print_char (48 + 8);
     print_char 32;
-    print_int image_size.(1);
+    (* print_int image_size.(1); *)
+    print_char (48 + 1);
+    print_char (48 + 2);
+    print_char (48 + 8);
     print_char 32;
-    print_int 255;
+    (* print_int 255; *)
+    print_char (48 + 2);
+    print_char (48 + 5);
+    print_char (48 + 5);
     print_char 10
   )
 in
@@ -1935,16 +1944,13 @@ in
 let rec write_rgb_element x =
   let ix = int_of_float x in
   let elem = if ix > 255 then 255 else if ix < 0 then 0 else ix in
-  print_int elem
+  print_char elem
 in
 
 let rec write_rgb _ =
    write_rgb_element rgb.(0); (* Red   *)
-   print_char 32;
    write_rgb_element rgb.(1); (* Green *)
-   print_char 32;
-   write_rgb_element rgb.(2); (* Blue  *)
-   print_char 10
+   write_rgb_element rgb.(2)  (* Blue  *)
 in
 
 (******************************************************************************
@@ -2305,6 +2311,6 @@ let rec rt size_x size_y =
 )
 in
 
-let _ = rt 512 512
+let _ = rt 128 128 (* この数字(画像サイズ)を変更したらwrite_ppm_headerも変更すること *)
 
 in 0
