528b56bcce0d289b812aeb1e142ae4314e58e5e6
sueki743 <sueki743@users.noreply.github.com>
2016-12-01 15:51:31 +0900

    ライブラリ

--- mylib.ml
+++ mylib.ml
@@ -1,3 +1,9 @@
+let rec fless x y = x < y in
+let rec fispos x = x > 0.0 in
+let rec fisneg x = x < 0.0 in
+let rec fiszero x = (x = 0.0) in
+let rec fhalf x = x *. 0.5 in
+let rec fsqr x = x *. x in
 let rec cos x =
 	1.0 -. x *. x /. 2.0 +. x *. x *. x *. x /. 24.0
 in
